b2730d2d44106d51f6348f14bafbd9b1b9c74299
James <jselby@jselby.net>
2015-01-28 19:54:49 +1100

    Add boilerplate Maven configuration

---
+++ src/main/java/net/cubition/launcher/Launcher.java
@@ -0,0 +1,11 @@
+package net.cubition.launcher;
+
+/**
+ * The Cubition Launcher eases the login and Bootstrap process for the Client. This class specifically
+ * starts a Swing prompt, asking for user credentials, and gets Bootstrap ready.
+ */
+public class Launcher {
+    public static void main(String[] args) {
+
+    }
+}
