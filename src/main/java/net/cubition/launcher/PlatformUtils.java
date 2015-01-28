19b10e73700885c1e40daeda5ce513bf37ee1157
beeselmane <beeselmane@gmail.com>
2015-01-28 14:57:38 -0500

    Use OS X Standard Data Folder

--- src/main/java/net/cubition/launcher/PlatformUtils.java
+++ src/main/java/net/cubition/launcher/PlatformUtils.java
@@ -8,8 +8,11 @@ import java.io.File;
 public class PlatformUtils {
     public static File getApplicationData() {
         String os = System.getProperty("os.name").toUpperCase();
+
         if (os.contains("WIN")) {
             return new File(System.getenv("AppData"));
+        } else if (os.contains("OS X")) {
+            return new File(System.getProperty("user.home") + "/Library/Application Support/");
         } else {
             return new File(System.getProperty("user.home"));
         }
